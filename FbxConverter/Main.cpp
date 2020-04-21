36e33464e1b68cca28d2d8ed35fa71f073b6c6c7
daiki_suzuki <2015daikisuzuki@gmail.com>
2020-04-21 17:35:24 +0900

    マテリアル、テクスチャに対応

--- FbxConverter/Main.cpp
+++ FbxConverter/Main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
@@ -13,19 +14,43 @@ struct Vertex
 {
 	float position[3];
 	float normal[3];
-	//float textureCoord[2];
+	float textureCoord[2];
+};
+
+struct Subset
+{
+	int mat_index;
+	int vertexCount;
+	int vertexStart;
+};
+
+struct Color
+{
+	float color[3];
+};
+
+struct Material
+{
+	int id;
+	float diffuse[3];
+	float alpha;
+	float ambient[3];
+	float specular[3];
+	float power;
+	float emmisive[3];
+
+	string texture_name;
 };
 
 struct Mesh
 {
-	int vartexCount;
-	int* indexArray;
-	Vertex* verteices;
+	vector<int> indexArray;
+	vector<Vertex> verteices;
+	vector<Subset> subset;
+	vector<Material> material;
 };
 
-struct Model
-{
-};
+Mesh g_mesh;
 
 void ChangeTriangle(FbxScene* scene,FbxNode* node);
 void PrintNode(FbxNode* node,int tab);
@@ -35,10 +60,18 @@ void PrintNormal(FbxMesh* mesh);
 int GetNormalIndex(FbxGeometryElementNormal* layer,int index);
 void PrintUV(FbxMesh* mesh);
 void PrintMaterial(FbxSurfaceMaterial* material);
-void PrintMaterialProperty(FbxSurfaceMaterial* material,const char* prop,const char* propFactory);
+Color PrintMaterialProperty(FbxSurfaceMaterial* material,const char* prop,const char* propFactory,string* textureName);
 
-int main()
+void SaveMesh(const char* name);
+
+int main(int argc, char** args)
 {
+	if(argc <  2)
+	{
+		cout << "引数が足りません。ファイルを指定してください" << endl;
+		return -1;
+	}
+
 	FbxManager* fbxManager = FbxManager::Create();
 
 	FbxIOSettings* ioSetting = FbxIOSettings::Create(fbxManager,IOSROOT);
@@ -49,7 +82,8 @@ int main()
 	//FbxString fileName("model1.fbx");
 	//FbxString fileName("sadface.fbx");
 	//FbxString fileName("Normals.fbx");
-	FbxString fileName("SD_unitychan_humanoid.fbx");
+	//FbxString fileName("SD_unitychan_humanoid.fbx");
+	FbxString fileName(args[1]);
 	auto fbxImporter = FbxImporter::Create(fbxManager,"imp");
 	if(!fbxImporter->Initialize(fileName.Buffer(),-1,fbxManager->GetIOSettings()))
 	{
@@ -101,6 +135,8 @@ int main()
 
 	fbxManager->Destroy();
 
+	SaveMesh(args[1]);
+
 	system("pause");
 
 	return 0;
@@ -135,18 +171,19 @@ void PrintNode(FbxNode* node,int tab)
 {
 	for(int i = 0;i < tab;i++)
 	{
-		cout << " ";
+		//cout << " ";
 	}
-	cout << node->GetName() << " ";
+	//cout << node->GetName() << " ";
 
 	for(int i = 0;i < node->GetNodeAttributeCount();i++)
 	{
 		FbxNodeAttribute* nodeAttribte = node->GetNodeAttribute();
 		auto type = nodeAttribte->GetAttributeType();
 		int materialCount = node->GetMaterialCount();
-		cout << nodeAttribte->GetName() << " " << GetNodeAttributeName(type) << ",MaterialCount:" << materialCount;
+		//cout << nodeAttribte->GetName() << " " << GetNodeAttributeName(type) << ",MaterialCount:" << materialCount;
 		for(int k = 0;k < materialCount;k++)
 		{
+			cout << "Material";
 			PrintMaterial(node->GetMaterial(k));
 		}
 		if(type == FbxNodeAttribute::eMesh)
@@ -155,22 +192,6 @@ void PrintNode(FbxNode* node,int tab)
 			if(mesh)
 			{
 				PrintVertex(mesh);
-				/*int count = mesh->GetControlPointsCount();
-				cout << "\nControlPointsCount:" << count << endl;
-				for(int k = 0;k < count;k++)
-				{
-					auto pos = mesh->GetControlPointAt(k);
-					cout << k << ":(" << pos[0] << "," << pos[1] << "," << pos[2] << ")" << endl;
-				}
-				count = mesh->GetPolygonVertexCount();
-				cout << "PolygonVertexCount:" << count << endl;
-				auto vertices = mesh->GetPolygonVertices();
-				for(int k = 0;k < count;k++)
-				{
-					cout << k << ":" << vertices[k]  << endl;
-				}
-				PrintNormal(mesh);
-				PrintUV(mesh);*/
 			}
 		}
 	}
@@ -259,117 +280,106 @@ void PrintVertex(FbxMesh* mesh)
 		}
 	}*/
 
-	Mesh meshData;
+	FbxLayerElementArrayTemplate<int>* materialArray;
+	int materialCount = 0;
+	if(mesh->GetMaterialIndices(&materialArray))
+	{
+		materialCount = materialArray->GetCount();
+		/*for(int i = 0;i < polygonCount;i++)
+		{
+			cout << materialArray->GetAt(i) << endl;
+		}*/
+	}
 
-	meshData.vartexCount = mesh->GetPolygonVertexCount();
-	meshData.verteices = new Vertex[meshData.vartexCount];
-	meshData.indexArray = new int[meshData.vartexCount];
+
+	int vartexCount = mesh->GetPolygonVertexCount();
+	g_mesh.verteices.reserve( g_mesh.verteices.size() + vartexCount );
+	g_mesh.indexArray.reserve( g_mesh.indexArray.size() + vartexCount );
+
+	int indexStart = g_mesh.indexArray.size();
+
+	int matCount = mesh->GetNode()->GetMaterialCount();
+	if( matCount <= 0 )
+	{
+		matCount = 1;
+	}
+	vector<int>* indexArray = new vector<int>[matCount];
+
 	int count = 0;
+	int indexPos = 0;
+	int startArray = g_mesh.indexArray.size();
+	Vertex vertex;
 	for(int i = 0;i < polygonCount;i++)
 	{
-		int polygonSize = mesh->GetPolygonSize(i);
-		if( polygonSize != 3 )
-		{
-			cout << "polygonSize:" << polygonSize << endl;
+		if( i < materialCount)
+		{	
+			indexPos = materialArray->GetAt(i);
+			//cout << indexPos << endl;
 		}
+		int polygonSize = mesh->GetPolygonSize(i);
 		for(int k = 0;k < polygonSize;k++)
 		{
 			int index = mesh->GetPolygonVertex(i,k);
-			meshData.indexArray[count] = count;
+			indexArray[indexPos].push_back( startArray + count );
+			//g_mesh.indexArray.push_back( count );
 			auto pos = mesh->GetControlPointAt(index);
 			FbxVector4 normal;
 			mesh->GetPolygonVertexNormal(i,k,normal);
 
-			/*cout << "Vertex Data "  << i << "," << k << ":";
-			cout << "Pos:(";*/
 			for(int m = 0;m < 3;m++)
 			{
-				meshData.verteices[count].position[m] = pos[m];
-				//cout << pos[m] << ",";
+				vertex.position[m] = pos[m];
+				vertex.normal[m] = normal[m];
 			}
-			//cout << ") ";
 
-			//cout << "Normal:(";
-			for(int m = 0;m < 3;m++)
-			{
-				meshData.verteices[count].normal[m] = normal[m];
-				//cout << normal[m] << ",";
-			}
-			//cout << ") ";
-
-			/*for(int n = 0;n < uvSetCount;n++)
+			for(int n = 0;n < uvSetCount;n++)
 			{
 				const char* setName = uvSetName.GetStringAt(n);
 				
 				FbxVector2 textureCoord;
 				mesh->GetPolygonVertexUV(i,k,setName,textureCoord,unmaped);
-				cout << "UV" << n << ":(";
 				for(int m = 0;m < 2;m++)
 				{
-					meshData.verteices[count].textureCoord[m] = textureCoord[m];
-					cout << textureCoord[m] << ",";
+					vertex.textureCoord[m] = textureCoord[m];
 				}
-				cout << ") ";
-			}*/
+			}
 
-			//cout << endl;
-			
+			g_mesh.verteices.push_back(vertex);
+
 			count++;
 		}
 	}
-
-	ofstream file("model.gmb",ios::binary);
-
-	if(!file.is_open())
-	{
-		return;
-	}
-	file.write(reinterpret_cast<const char*>(&meshData.vartexCount),sizeof(int));
-	for(int i = 0;i < meshData.vartexCount;i++)
-	{
-		file.write(reinterpret_cast<const char*>(meshData.verteices[i].position),sizeof(float) * 3);
-	}
-	for(int i = 0;i < meshData.vartexCount;i++)
-	{
-		file.write(reinterpret_cast<const char*>(meshData.verteices[i].normal),sizeof(float) * 3);
-	}
-	/*for(int i = 0;i < meshData.vartexCount;i++)
-	{
-		file.write(reinterpret_cast<const char*>(meshData.verteices[i].textureCoord),sizeof(float) * 2);
-	}*/
-	file.write(reinterpret_cast<const char*>(meshData.indexArray),sizeof(int) * meshData.vartexCount);
-	file.close();
-
-	ofstream dfile("model.gmd");
-
-	if(!dfile.is_open())
-	{
-		return;
-	}
-	dfile << "VertexCount:" << meshData.vartexCount << endl;
 	
-	dfile << "Position:" << endl;
-	for(int i = 0;i < meshData.vartexCount;i++)
+	//cout << "array" << endl;
+	for(int i = 0;i < matCount;i++)
 	{
-		dfile << "(" << meshData.verteices[i].position[0] << "," << meshData.verteices[i].position[1] << "," << meshData.verteices[i].position[2] << ")" << endl;
+		Subset subset;
+		subset.mat_index = -1;
+		auto mat = mesh->GetNode()->GetMaterial(i);
+		if( mat != nullptr )
+		{
+			int id = mat->GetUniqueID();
+			for(int k = 0;k < g_mesh.material.size();k++)
+			{
+				if(g_mesh.material[k].id == id)
+				{
+					subset.mat_index = k;
+				}
+			}
+		}
+
+		subset.vertexStart = g_mesh.indexArray.size();
+		subset.vertexCount = indexArray[i].size();
+		g_mesh.subset.push_back(subset);
+		for(int k = 0;k < subset.vertexCount;k++)
+		{
+			//cout << i << ",";
+			g_mesh.indexArray.push_back(indexArray[i][k]);
+		}
+		//cout << endl;
 	}
-	dfile << "Normal:" << endl;
-	for(int i = 0;i < meshData.vartexCount;i++)
-	{
-		dfile << "(" << meshData.verteices[i].normal[0] << "," << meshData.verteices[i].normal[1] << "," << meshData.verteices[i].normal[2] << ")" << endl;
-	}
-	/*dfile << "TextureCoord:" << endl;
-	for(int i = 0;i < meshData.vartexCount;i++)
-	{
-		dfile << "(" << meshData.verteices[i].textureCoord[0] << "," << meshData.verteices[i].textureCoord[1] << ")" << endl;
-	}*/
-	dfile << "indexArray:" << endl;
-	for(int i = 0;i < meshData.vartexCount;i++)
-	{
-		dfile << meshData.indexArray[i] << ",";
-	}
-	dfile << endl;
-	dfile.close();
+
+	delete[] indexArray;
 }
 
 void PrintNormal(FbxMesh* mesh)
@@ -489,35 +499,83 @@ void PrintUV(FbxMesh* mesh)
 
 void PrintMaterial(FbxSurfaceMaterial* material)
 {
-	cout << material->GetClassId().GetName();
-	if(material->GetClassId().Is(FbxSurfaceLambert::ClassId))
+	int id = material->GetUniqueID();
+	for(int i = 0;i < g_mesh.material.size();i++)
 	{
-		cout << " Lambert";
+		if(g_mesh.material[i].id == id)
+		{
+			return;
+		}
 	}
-	else if(material->GetClassId().Is(FbxSurfacePhong::ClassId))
+	Material mat;
+	mat.id = id;
+	
+	
+	cout << material->GetName() << endl;
+	cout << material->GetClassId().GetName();
+	if(material->GetClassId().Is(FbxSurfacePhong::ClassId))
 	{
 		cout << " Phong";
 	}
+	else if(material->GetClassId().Is(FbxSurfaceLambert::ClassId))
+	{
+		cout << " Lambert";
+	}
+
 	cout << endl;
-	PrintMaterialProperty(material,FbxSurfaceMaterial::sDiffuse,FbxSurfaceMaterial::sDiffuseFactor);
-	PrintMaterialProperty(material,FbxSurfaceMaterial::sAmbient,FbxSurfaceMaterial::sAmbientFactor);
-	PrintMaterialProperty(material,FbxSurfaceMaterial::sEmissive,FbxSurfaceMaterial::sEmissiveFactor);
-	PrintMaterialProperty(material,FbxSurfaceMaterial::sSpecular,FbxSurfaceMaterial::sSpecularFactor);
+	string textureName;
+	Color color = PrintMaterialProperty(material,FbxSurfaceMaterial::sDiffuse,FbxSurfaceMaterial::sDiffuseFactor,&textureName);
+	for(int i = 0;i < 3;i++)
+	{
+		mat.diffuse[i] = color.color[i];
+	}
+	color = PrintMaterialProperty(material,FbxSurfaceMaterial::sAmbient,FbxSurfaceMaterial::sAmbientFactor,&textureName);
+	for(int i = 0;i < 3;i++)
+	{
+		mat.ambient[i] = color.color[i];
+	}
+	color = PrintMaterialProperty(material,FbxSurfaceMaterial::sEmissive,FbxSurfaceMaterial::sEmissiveFactor,&textureName);
+	for(int i = 0;i < 3;i++)
+	{
+		mat.emmisive[i] = color.color[i];
+	}
+	color = PrintMaterialProperty(material,FbxSurfaceMaterial::sSpecular,FbxSurfaceMaterial::sSpecularFactor,&textureName);
+	for(int i = 0;i < 3;i++)
+	{
+		mat.specular[i] = color.color[i];
+	}
+	if( !textureName.empty() )
+	{
+		auto pos = textureName.rfind("\\");
+		if(pos != string::npos)
+		{
+			textureName.erase(0,pos+1);
+		}
+		mat.texture_name = textureName.c_str();
+		cout << "Texture Name:" << textureName.c_str() << endl;
+	}
 
 	FbxProperty transparency = material->FindProperty(FbxSurfaceMaterial::sTransparencyFactor);
+	mat.alpha = 1.0f;
 	if(transparency.IsValid())
 	{
-		cout << FbxSurfaceMaterial::sTransparencyFactor << ":" << transparency.Get<FbxDouble>() << endl;
+		mat.alpha = 1.0f - static_cast<float>(transparency.Get<FbxDouble>());
+		cout << FbxSurfaceMaterial::sTransparencyFactor << ":" << transparency.Get<FbxDouble>() << " " << mat.alpha << endl;
 	}
 	FbxProperty shininess = material->FindProperty(FbxSurfaceMaterial::sShininess);
+	mat.power = 0.0f;
 	if(shininess.IsValid())
 	{
+		mat.power = static_cast<float>(shininess.Get<FbxDouble>());
 		cout << FbxSurfaceMaterial::sShininess << ":" << shininess.Get<FbxDouble>() << endl;
 	}
+
+	g_mesh.material.push_back(mat);
 }
 
-void PrintMaterialProperty(FbxSurfaceMaterial* material,const char* prop,const char* propFactory)
+Color PrintMaterialProperty(FbxSurfaceMaterial* material,const char* prop,const char* propFactory,string* textureName)
 {
+	Color result = {0.0f,0.0f,0.0f};
 	FbxProperty fProp = material->FindProperty(prop);
 	FbxProperty fPropFactory = material->FindProperty(propFactory);
 	if(fProp.IsValid()&&fPropFactory.IsValid())
@@ -525,6 +583,10 @@ void PrintMaterialProperty(FbxSurfaceMaterial* material,const char* prop,const c
 		FbxDouble3 color = fProp.Get<FbxDouble3>();
 		double k = fPropFactory.Get<FbxDouble>();
 		cout << " " << prop << ":(" << color[0] << "," << color[1] << "," << color[2] << ") " << propFactory << ":" << k << endl;
+		for(int i = 0;i < 3;i++)
+		{
+			result.color[i] = color[i] * k;
+		}
 	}
 
 	if( fProp.IsValid() )
@@ -539,6 +601,7 @@ void PrintMaterialProperty(FbxSurfaceMaterial* material,const char* prop,const c
 			}
 			FbxString uvsetName = texture->UVSet.Get();
 			cout <<"texture:" << i << " " << uvsetName.Buffer() << "," << texture->GetFileName() << endl;
+			*textureName = texture->GetFileName();
 		}
 
 		textureCount = fProp.GetSrcObjectCount<FbxLayeredTexture>();
@@ -555,9 +618,103 @@ void PrintMaterialProperty(FbxSurfaceMaterial* material,const char* prop,const c
 				}
 				FbxString uvsetName = texture->UVSet.Get();
 				cout <<"LayeredTexture:" << i << " " << uvsetName.Buffer() << "," << texture->GetFileName() << endl;
+				*textureName = texture->GetFileName();
 			}
 		}
 	}
+
+	return result;
 }
 
+void SaveMesh(const char* name)
+{
+	
+	string fileName(name);
+	auto pos = fileName.rfind("\\");
+	if(pos != string::npos)
+	{
+		fileName.erase(0,pos+1);
+	}
+	pos = fileName.rfind(".");
+	if(pos != string::npos)
+	{
+		fileName.erase(pos,4);
+	}
+	cout << fileName.c_str();
+	ofstream file(fileName + ".gmb", ios::out|ios::binary);
 
+	if(!file.is_open())
+	{
+		return;
+	}
+	int vertexCount = g_mesh.verteices.size();
+	int indexCount = g_mesh.indexArray.size();
+	int subsetCount = g_mesh.subset.size();
+	int materialCount = g_mesh.material.size();
+	file.write(reinterpret_cast<const char*>(&vertexCount),sizeof(int));
+	file.write(reinterpret_cast<const char*>(&indexCount),sizeof(int));
+	file.write(reinterpret_cast<const char*>(&subsetCount),sizeof(int));
+	file.write(reinterpret_cast<const char*>(&materialCount),sizeof(int));
+	file.write(reinterpret_cast<const char*>(&g_mesh.verteices[0]),sizeof(Vertex) * vertexCount);
+	file.write(reinterpret_cast<const char*>(&g_mesh.indexArray[0]),sizeof(int) * indexCount);
+	file.write(reinterpret_cast<const char*>(&g_mesh.subset[0]),sizeof(Subset) * subsetCount);
+	for(int i = 0;i < materialCount;i++)
+	{
+		file.write(reinterpret_cast<const char*>(g_mesh.material[i].diffuse),sizeof(float) * 3);
+		file.write(reinterpret_cast<const char*>(&g_mesh.material[i].alpha),sizeof(float));
+		file.write(reinterpret_cast<const char*>(g_mesh.material[i].ambient),sizeof(float) * 3);
+		file.write(reinterpret_cast<const char*>(g_mesh.material[i].specular),sizeof(float) * 3);
+		file.write(reinterpret_cast<const char*>(&g_mesh.material[i].power),sizeof(float));
+		file.write(reinterpret_cast<const char*>(g_mesh.material[i].emmisive),sizeof(float) * 3);
+		int nameLength = g_mesh.material[i].texture_name.size() + 1;
+		file.write(reinterpret_cast<const char*>(&nameLength),sizeof(int));
+		file.write(g_mesh.material[i].texture_name.c_str(),nameLength);
+	}
+
+
+	file.close();
+
+	ofstream dfile(fileName + ".gmd");
+
+	if(!dfile.is_open())
+	{
+		return;
+	}
+	dfile << "Vertex Count:" << vertexCount << endl;
+	for(int k = 0;k < vertexCount;k++)
+	{
+		dfile << "(" << g_mesh.verteices[k].position[0] << "," << g_mesh.verteices[k].position[1] << "," << g_mesh.verteices[k].position[2] << ") ";
+		dfile << "(" << g_mesh.verteices[k].normal[0] << "," << g_mesh.verteices[k].normal[1] << "," << g_mesh.verteices[k].normal[2] << ") ";
+		dfile << "(" << g_mesh.verteices[k].textureCoord[0] << "," << g_mesh.verteices[k].textureCoord[1] << ") " << endl;
+	}
+
+	dfile << "Index Count:" << indexCount << endl;
+	for(int k = 0;k < indexCount;k++)
+	{
+		dfile << g_mesh.indexArray[k] << ",";
+	}
+	dfile << endl;
+
+	dfile << "Subset Count:" << subsetCount << endl;
+	for(int k = 0;k < subsetCount;k++)
+	{
+		dfile << "Material Index:" << g_mesh.subset[k].mat_index << " Vertex Start:" << g_mesh.subset[k].vertexStart << " Vertex Count:" << g_mesh.subset[k].vertexCount << endl;
+	}
+
+	dfile << "Material Count:" << materialCount << endl;
+	for(int k = 0;k < materialCount;k++)
+	{
+		Material& mat = g_mesh.material[k];
+		dfile << "Material" << k << endl;
+		dfile << "Diffuse Color(" << mat.diffuse[0] << "," << mat.diffuse[1] << "," << mat.diffuse[2] << ")" << endl;
+		dfile << "Alpah:" << mat.alpha << endl;
+		dfile << "Ambient Color(" << mat.ambient[0] << "," << mat.ambient[1] << "," << mat.ambient[2] << ")" << endl;
+		dfile << "Specular Color(" << mat.specular[0] << "," << mat.specular[1] << "," << mat.specular[2] << ")" << endl;
+		dfile << "Power:" << mat.power << endl;
+		dfile << "Emmisive Color(" << mat.emmisive[0] << "," << mat.emmisive[1] << "," << mat.emmisive[2] << ")" << endl;
+		dfile << "Texture Name:" << mat.texture_name.c_str() << endl;
+		
+	}
+
+	dfile.close();
+}
